3fd045d2b2dccff9ad10c0bf5f4727719588944f
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-09-27 14:57:07 +0500

    main

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from citizen-service-navigator-ai!")
+from fastapi import FastAPI
+from routers import eligibility_router
+
+app = FastAPI()
+
+app.include_router(eligibility_router.router)
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to the Citizen Service Navigator AI API!"}
