1d514b0d14fc5477accca0eacdad873b7e35ed24
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-10-05 17:47:47 +0500

    Update

--- helpers.py
+++ helpers.py
@@ -1,11 +1,14 @@
 from database.mongo_connection import get_citizen_data, get_service_data
+from agents import function_tool
 
 
+@function_tool
 def fetch_citizen_data(citizen_id: str):
     citizen_data = get_citizen_data(citizen_id)
     return citizen_data
 
 
+@function_tool
 def fetch_service_data(service_id: str):
     service_data = get_service_data(service_id)
     return service_data
